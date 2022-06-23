d11653bdd6ba8c0fd2bdbba2869c6e820e3d8c34
Nicolas Milhem <nicolasmilhem@gmail.com>
2022-06-23 16:41:06 +0200

    ProgramListDash - ajout icônes boutons

--- src/components/ProgramListDash.vue
+++ src/components/ProgramListDash.vue
@@ -21,56 +21,87 @@
                     <td>{{ program.platform }}</td>
                     <td>
                         <button @click="setProgramSelected" v-bind:data-programid="program._id" type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal"
-                            data-bs-target="#exampleModal">
-                            Voir
+                            data-bs-target="#modalView">
+                            <i class="bi bi-eye d-flex align-items-center"></i>
+                            <!-- Voir -->
                         </button>
                     </td>
-                    <td>Modifier</td>
                     <td>
-                        <button @click="deleteProg" v-bind:data-programid="program._id" type="button" class="btn btn-danger btn-sm">Supprimer</button>
+                        <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal"
+                            data-bs-target="#modalUpdate">
+                            <i class="bi bi-pencil d-flex align-items-center"></i>
+                            <!-- Modifier -->
+                        </button>
+                    </td>
+                    <td>
+                        <button @click="deleteProg" v-bind:data-programid="program._id" type="button" class="btn btn-danger btn-sm">
+                            <i class="bi bi-trash d-flex align-items-center"></i>
+                            <!-- Supprimer -->
+                        </button>
                     </td>
                 </tr>
             </tbody>
             
         </table>
-        <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h3 class="modal-title" id="exampleModalLabel">Programme</h3>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                aria-label="Close"></button>
+
+        <!-- Modal View -->
+        <div class="modal fade" id="modalView" tabindex="-1" aria-labelledby="modalViewLabel"
+            aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h3 class="modal-title" id="modalViewLabel">Programme</h3>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                            aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body d-flex  gap-4">
+                        <div class="body-left fw-bold me-10 d-flex flex-column align-items-end">
+                            <p>DATE</p>
+                            <p>TITRE</p>
+                            <p>SAISON</p>
+                            <p>PLATEFORME</p>
+                            <p>PAYS</p>
+                            <p>NOTE</p>
                         </div>
-                        <div class="modal-body d-flex  gap-4">
-                            <div class="body-left fw-bold me-10 d-flex flex-column align-items-end">
-                                <p>DATE</p>
-                                <p>TITRE</p>
-                                <p>SAISON</p>
-                                <p>PLATEFORME</p>
-                                <p>PAYS</p>
-                                <p>NOTE</p>
+                        <div class="body-right">
+                            <p class="fw-bold text-danger">{{ formatTime(modalSchedule) }}</p>
+                            <p class="fw-bold text-primary">{{ modalTitle }}</p>
+                            <p>{{ modalSeason }}</p>
+                            <p>{{ modalPlatform }}</p>
+                            <div class="countries-list">
+                                <span v-for="country in modalCountries" :key="country">{{ country }}</span>
                             </div>
-                            <div class="body-right">
-                                <p class="fw-bold text-danger">{{ formatTime(modalSchedule) }}</p>
-                                <p class="fw-bold text-primary">{{ modalTitle }}</p>
-                                <p>{{ modalSeason }}</p>
-                                <p>{{ modalPlatform }}</p>
-                                <div class="countries-list">
-                                    <span v-for="country in modalCountries" :key="country">{{ country }}</span>
-                                </div>
-                                <p>{{ modalNote }}</p>
-                            </div>
+                            <p>{{ modalNote }}</p>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary"
-                                data-bs-dismiss="modal">Fermer
-                            </button>
-                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary"
+                            data-bs-dismiss="modal">Fermer
+                        </button>
                     </div>
                 </div>
             </div>
+        </div>
+
+        <!-- Modal Update -->
+        <div class="modal fade" id="modalUpdate" tabindex="-1" aria-labelledby="modalUpdateLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalUpdateLabel">Modification du programme</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -101,8 +132,10 @@
             },
             setProgramSelected(e) {
                 this.programList = this.$store.state.programs;
-                // console.log('this.programList', this.programList);
-                const pgSelected = this.programList.filter(program => program._id == e.target.dataset.programid)[0];
+                console.log('this.programList', this.programList);
+                // console.log(e.target.dataset.programid)
+                console.log(e.currentTarget.getAttribute('data-programid'))
+                const pgSelected = this.programList.filter(program => program._id == e.currentTarget.getAttribute('data-programid'))[0];
                 console.log('pgSelected', pgSelected);
 
                 this.modalSchedule = pgSelected.schedule;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "./assets/custom.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 import { createApp } from 'vue'
 import App from './App.vue'
