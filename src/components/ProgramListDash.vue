0437a4fe14cc9f4584ee03ad5859b7258c5371e9
Nicolas Milhem <nicolasmilhem@gmail.com>
2022-06-30 14:54:23 +0200

    ProgramListDash - fix problème timezone dans input

--- src/components/ProgramListDash.vue
+++ src/components/ProgramListDash.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container mt-3">
         <h2 class="text-center">Liste des programmes</h2>
-        <table class="table table-striped table-hover table-borderless align-middle">
+        <table class="table table-striped table-hover table-borderless align-middle fs-5">
             <thead>
                 <tr>
                     <th scope="col">Date</th>
@@ -27,7 +27,7 @@
                         </button>
                     </td>
                     <td>
-                        <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal"
+                        <button @click="updateProg" v-bind:data-programid="program._id" type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal"
                             data-bs-target="#modalUpdate">
                             <i class="bi bi-pencil d-flex align-items-center"></i>
                             <!-- Modifier -->
@@ -88,15 +88,41 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="modalUpdateLabel">Modification du programme</h5>
+                    <h3 class="modal-title" id="modalUpdateLabel">Modification du programme</h3>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    <form @submit.prevent="modifyProgram">
+                        <div class="mb-3">
+                            <label for="inputSchedule" class="form-label">Date</label>
+                            <input v-model="updateSchedule" type="datetime-local" class="form-control" id="inputSchedule">
+                        </div>
+                        <div class="mb-3">
+                            <label for="inputTitle" class="form-label">Titre</label>
+                            <input v-model="updateTitle" type="text" class="form-control" id="inputTitle">
+                        </div>
+                        <div class="mb-3">
+                            <label for="inputSeason" class="form-label">Saison</label>
+                            <input v-model="updateSeason" type="text" class="form-control" id="inputSeason">
+                        </div>
+                        <div class="mb-3">
+                            <label for="inputPlatform" class="form-label">Plateforme</label>
+                            <input v-model="updatePlatform" type="text" class="form-control" id="inputPlatform">
+                        </div>
+                        <div class="mb-3">
+                            <label for="inputCountries" class="form-label">Pays</label>
+                            <input v-model="updateCountries" type="text" class="form-control" id="inputCountries">
+                        </div>
+                        <div class="mb-3">
+                            <label for="inputNote" class="form-label">Note</label>
+                            <textarea v-model="updateNote" type="text" class="form-control" id="inputNote"/>
+                        </div>
+                        
+                        <button type="submit" class="btn btn-primary">Modifier</button>
+                    </form>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
                 </div>
                 </div>
             </div>
@@ -123,12 +149,19 @@
                 modalSeason: '',
                 modalPlatform: '',
                 modalCountries: [],
-                modalNote: ''
+                modalNote: '',
+                updateSchedule: null,
+                updateTitle: '',
+                updateSeason: '',
+                updatePlatform: '',
+                updateCountries: [],
+                updateNote: '',
+                programId: null
             }
         },
         methods: {
             formatTime(time) {
-                return moment(time).format('L')
+                return moment(time).format('L')+ ' - ' + moment(time).format('LT')
             },
             setProgramSelected(e) {
                 this.programList = this.$store.state.programs;
@@ -137,7 +170,6 @@
                 console.log(e.currentTarget.getAttribute('data-programid'))
                 const pgSelected = this.programList.filter(program => program._id == e.currentTarget.getAttribute('data-programid'))[0];
                 console.log('pgSelected', pgSelected);
-
                 this.modalSchedule = pgSelected.schedule;
                 this.modalTitle = pgSelected.title;
                 this.modalSeason = pgSelected.season;
@@ -167,8 +199,76 @@
 
                     alert('Programme supprimé !')
 
-                    // redirection vers la page d'inscription
-                    // this.$router.push('/signup');
+                } catch (error) {
+                    console.log(error.response.data);
+                }
+            },
+            updateProg(e) {
+                console.log('update prog');
+                this.programList = this.$store.state.programs;
+                const pgSelected = this.programList.filter(program => program._id == e.currentTarget.getAttribute('data-programid'))[0];
+                console.log(pgSelected.schedule.substr(0, 10));
+                console.log(pgSelected.schedule)
+                const scheduleFromDB = pgSelected.schedule.slice(0, -1).slice(0,16);
+                console.log(scheduleFromDB)
+
+                // Heure dans BD en UTC -> ajout de 2h pour récupération bonne heure dans l'input
+
+                let newDate = new Date(scheduleFromDB);
+                console.log("newDate", newDate);
+                newDate.setHours(newDate.getHours() + 2);
+                console.log("newDate after setHours", newDate);
+
+                const strNewDate = moment(newDate).format('L').split('/').reverse().join('-') + 'T' + moment(newDate).format('LT');
+
+                console.log(strNewDate)
+
+                this.updateSchedule = strNewDate;
+                this.updateTitle = pgSelected.title;
+                this.updateSeason = pgSelected.season;
+                this.updatePlatform = pgSelected.platform;
+                this.updateCountries = pgSelected.countries;
+                this.updateNote = pgSelected.note;
+                this.programId = pgSelected._id;
+            },
+            async modifyProgram() {
+                console.log('nouvel horaire', this.updateSchedule);
+
+                console.log('this.programId', this.programId)
+
+                console.log(typeof this.updateCountries);
+
+                const progId = this.programId;
+                const token = this.$store.state.token;
+
+                const updatedProgram = {
+                    title: this.updateTitle,
+                    season: this.updateSeason,
+                    countries: typeof this.updateCountries === "string" ? this.updateCountries.split(',') : Object.values(this.updateCountries),
+                    schedule: this.updateSchedule.split('T').join(' '),
+                    platform: this.updatePlatform,
+                    note: this.updateNote
+                }
+
+                console.log(updatedProgram);
+
+                try {
+                    const response = await Api.put(`programs/${progId}`, updatedProgram, {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                            'Content-Type': 'application/json'
+                        }});
+
+                    console.log(response.data);
+
+
+                    // const updatedData = response.data.updatedData;
+                    // const updatedUser = Object.assign({...this.$store.state.user}, {...updatedData});
+
+                    // mise à jour de user dans le state
+                    // this.$store.dispatch('updatePrograms');
+
+                    alert('modification effectuée !');
 
                 } catch (error) {
                     console.log(error.response.data);
